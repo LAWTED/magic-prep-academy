c0e179ad4975f48b4d8d5d389b48aa0c972e6813
Lawted <lawtedwu@gmail.com>
2025-03-21 01:02:04 +0800

    Refactor StudentsLayout to conditionally render StudentHeader and BottomNavigation based on the current path. Improved mobile responsiveness by applying padding dynamically to the main content area.

--- app/(students)/layout.tsx
+++ app/(students)/layout.tsx
@@ -3,6 +3,7 @@
 import BottomNavigation from "@/components/BottomNavigation";
 import StudentHeader from "@/app/components/StudentHeader";
 import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 export default function StudentsLayout({
   children,
@@ -12,15 +13,21 @@ export default function StudentsLayout({
   const pathname = usePathname();
 
   // Check if the current path is one that should have the bottom navigation
-  const shouldShowNavigation = pathname === "/homepage" || pathname === "/awards";
+  const shouldShowNavigation =
+    pathname === "/homepage" || pathname === "/awards";
 
   // Check if the current path should have the header
-  const shouldShowHeader = pathname !== "/onboarding";
+  const shouldShowHeader = pathname === "/homepage" || pathname === "/awards";
 
   return (
     <div className="min-h-[100dvh] flex flex-col bg-background">
       {shouldShowHeader && <StudentHeader />}
-      <main className="flex-1 flex flex-col w-full max-w-screen-md mx-auto pb-16">
+      <main
+        className={cn(
+          "flex-1 flex flex-col w-full max-w-screen-md mx-auto ",
+          shouldShowNavigation && "pb-16"
+        )}
+      >
         {children}
       </main>
       {shouldShowNavigation && <BottomNavigation />}
